d91371b2c771e74619db9205a3194870bd8c87b7
Adam Sardar <adam@sardar.me.uk>
2013-03-27 11:24:05 +0000

    Fixed a bug to do with file handles

--- FM_read_omit.py
+++ FM_read_omit.py
@@ -11,18 +11,17 @@ def main():
 	parser.add_argument('--file', '-f', metavar='FILE', required=True, type=argparse.FileType('r'), dest='InputFile', help='File to trim reads of')
 	parser.add_argument('--output', '-o', metavar='OUTPUT', required=False, dest='Output', default='LongReads.fastq', type=argparse.FileType('w'), help='Output read file')
 	parser.add_argument('--length', '-l', metavar='LENGTH', required=False, dest='Length', default=30, type=int, help='Minimum number of nucleotides in read')
-	
+
 	args = parser.parse_args()
 
 	Reads = SeqIO.parse(args.InputFile, 'fastq')
 
 	# Generator to iterate through records and only return those that satisfy if ...
 	record_genetor = (rec for rec in Reads if len(rec) > args.Length)
-	
-	outfile = open(args.Output, 'wb')
-	numwritten = SeqIO.write(record_genetor, outfile, 'fastq')
 
-	print 'Trimmed {0} down to {1} reads'.format(f, numwritten)
+	numwritten = SeqIO.write(record_genetor, args.Output, 'fastq')
+
+	print 'Trimmed {0} down to {1} reads'.format(args.InputFile, numwritten)
 	
 
 if __name__ == "__main__":
